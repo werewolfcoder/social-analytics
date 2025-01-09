2810849bdd8aa8691ebd04da7943fae430ff93a0
werewolfcoder <shuklaarun199@gmail.com>
2025-01-09 16:46:57 +0530

    badmosi

--- lang-flow/vite.config.js
+++ lang-flow/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { json } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define:{
+    'process.env.FLOW_ID':JSON.stringify(process.env.FLOW_ID),
+    'process.env.LANGFLOW_ID':JSON.stringify(process.env.LANGFLOW_ID),
+    'process.env.APPLICATION_TOKEN':JSON.stringify(process.env.APPLICATION_TOKEN),
+    'process.env.GOOGLE_API_KEY':JSON.stringify(process.env.GOOGLE_API_KEY)
+  }
 })
